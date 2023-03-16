89b9aab2088623468c00395f3e9df2ea06d35d7a
cynth765 <cherocynth509@gmail.com>
2023-03-16 11:19:58 +0300

    navbar

--- src/components/Business.vue
+++ src/components/Business.vue
@@ -1,17 +1,23 @@
 <template>
-     <div class="p-4 sm:w-1/2 lg:w-1/4 ml-96" >
-                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                        <img class="mx-12 object-cover object-center"
-                            src="https://www.mgkconsult.co.ke/wp-content/uploads/2022/10/Audit-and-Assurance-services-in-kenya.webp" alt="blog">
-                        <div class="p-6  transition duration-300 ease-in">
+     <div class="section1 mt-6">
+    <h1>Business Services</h1>
+    <hr>
+
+  </div>
+     <div class="ml-60 mt-20" >
+                    <div class="">
+           
+                        <div class="">
                            
                             <h1 class="text-2xl font-semibold mb-3">Business Services</h1>
-                            <p class="leading-relaxed mb-3">It includes Business Valuation,Business registration,Annual returns,Share transfer, Change in directorship. We invest time to ensure that we understand your business; its organizational make-up,culture, strategic direction, performance goals and competitive position.</p>
+                            <p class="leading-relaxed mb-3">It includes Business Valuation,Business registration,Annual returns,<br>Share transfer, Change in directorship. We invest time to ensure that  <br>we understand your business; its organizational make-up,culture,<br> strategic direction, performance goals and competitive position.</p>
                            
                         </div>
-                        <button class="bg-blue-600  space-y-6 hover:bg-yellow-600  m-7 text-white font-bold py-2 px-4 rounded buton">
-                       Learn More
-                      </button>
+                        <router-link to="/contact">
+                        <button class="bg-blue-600  hover:bg-yellow-600  m-7 text-white font-bold py-2 px-4 rounded butn">
+                       Contact us
+                   </button>
+                </router-link>
                     </div>
                 </div>
 </template>
